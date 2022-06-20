1050aa8a213b430fcd11ea7fec7eb10f6034c07d
jaemak23 <macklin.jaison@gmail.com>
2022-06-20 15:16:57 +0530

    Final

--- app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
+++ app/src/main/java/com/example/android/databinding/basicsample/ui/PlainOldActivity.kt
@@ -27,10 +27,12 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.widget.ImageViewCompat
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.databinding.basicsample.R
 import com.example.android.databinding.basicsample.data.Popularity
 import com.example.android.databinding.basicsample.data.SimpleViewModel
+import com.example.android.databinding.basicsample.databinding.PlainActivityBinding
 
 /**
  * Plain old activity with lots of problems to fix.
@@ -38,16 +40,20 @@ import com.example.android.databinding.basicsample.data.SimpleViewModel
 class PlainOldActivity : AppCompatActivity() {
 
     // Obtain ViewModel from ViewModelProviders
-    private val viewModel by lazy { ViewModelProviders.of(this).get(SimpleViewModel::class.java) }
+    private val _viewModel by lazy { ViewModelProviders.of(this).get(SimpleViewModel::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.plain_activity)
+        val binding: PlainActivityBinding =DataBindingUtil.setContentView(this,R.layout.plain_activity)
+
+        binding.lifecycleOwner=this
+        binding.viewModel=_viewModel
+        //setContentView(R.layout.plain_activity)
 
         // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
-        updateName()
-        updateLikes()
+       // updateName()
+       // updateLikes()
     }
 
     /**
@@ -55,17 +61,13 @@ class PlainOldActivity : AppCompatActivity() {
      * logic in the activity, which is not ideal. We should do something about that.
      */
     fun onLike(view: View) {
-        viewModel.onLike()
-        updateLikes()
+        _viewModel.onLike()
+       // updateLikes()
     }
 
     /**
      * So much findViewById! We'll fix that with Data Binding.
      */
-    private fun updateName() {
-        findViewById<TextView>(R.id.plain_name).text = viewModel.name
-        findViewById<TextView>(R.id.plain_lastname).text = viewModel.lastName
-    }
 
     /**
      * This method has many problems:
@@ -73,18 +75,18 @@ class PlainOldActivity : AppCompatActivity() {
      * - It has untestable logic
      * - It's updating two views when called even if they're not changing
      */
-    private fun updateLikes() {
-        findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
-        findViewById<ProgressBar>(R.id.progressBar).progress =
-            (viewModel.likes * 100 / 5).coerceAtMost(100)
-        val image = findViewById<ImageView>(R.id.imageView)
-
-        val color = getAssociatedColor(viewModel.popularity, this)
-
-        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
-
-        image.setImageDrawable(getDrawablePopularity(viewModel.popularity, this))
-    }
+//    private fun updateLikes() {
+//        findViewById<TextView>(R.id.likes).text = viewModel.likes.toString()
+//        findViewById<ProgressBar>(R.id.progressBar).progress =
+//            (viewModel.likes * 100 / 5).coerceAtMost(100)
+//        val image = findViewById<ImageView>(R.id.imageView)
+//
+//        val color = getAssociatedColor(viewModel.popularity, this)
+//
+//        ImageViewCompat.setImageTintList(image, ColorStateList.valueOf(color))
+//
+//        image.setImageDrawable(getDrawablePopularity(viewModel.popularity, this))
+//    }
 
     private fun getAssociatedColor(popularity: Popularity, context: Context): Int {
         return when (popularity) {
